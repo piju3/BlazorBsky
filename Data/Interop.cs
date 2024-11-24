6e51fd8e09f0b833511cdd14f90a989f9c0ad6bf
piju3 <none@example.com>
2024-11-24 22:06:29 +0100

    v0.1

--- Data/Interop.cs
+++ Data/Interop.cs
@@ -1,6 +1,7 @@
 using System.Runtime.InteropServices.JavaScript;
 namespace BlazorBsky
 {
+    /*
     internal static partial class LocalStorage
     {
         //https://developer.mozilla.org/en-US/docs/Web/API/Storage
@@ -10,37 +11,5 @@ namespace BlazorBsky
         [JSImport("globalThis.localStorage.setItem")]
         public static partial void SetItem(string key, string value);
     }
-
-    /// <summary>
-    /// This class will use <see href="https://developer.mozilla.org/en-US/docs/Web/API/Window/requestAnimationFrame">requestAnimationFrame</see>
-    /// to call a method every render frame of the page. If the page is not visible, it will not be called.
-    /// </summary>
-    internal partial class AnimationFrameCallback : IDisposable
-    {
-        [JSImport("globalThis.requestAnimationFrame")]
-        private static partial int RequestAnimationFrame([JSMarshalAs<JSType.Function<JSType.Number>>()] Action<double> method);
-
-        private readonly Action<double> callback;
-        private bool disposed = false;
-
-        private void Handler(double timestamp)
-        {
-            callback(timestamp);
-            if (!disposed)
-            {
-                _ = RequestAnimationFrame(Handler);
-            }
-        }
-        public AnimationFrameCallback(Action<double> callback)
-        {
-            this.callback = callback;
-            _ = RequestAnimationFrame(Handler);
-        }
-        //Remember to dispose of the object to remove the callback.
-        public void Dispose()
-        {
-            disposed = true;
-            GC.SuppressFinalize(this);
-        }
-    }
+    */
 }
